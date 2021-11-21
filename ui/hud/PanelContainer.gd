d8b194b35386bbec0527f6b22f0b26861e0b992a
Gabriel Minossi <gabriel.minossi@gmail.com>
2021-11-21 16:18:51 -0300

    Refactoring HUD Anchoring elements to screen corners and edges Removing hard-coded spell names Removing show spells button Making selected spells appear as selected

--- ui/hud/PanelContainer.gd
+++ ui/hud/PanelContainer.gd
@@ -2,12 +2,13 @@ extends PanelContainer
 
 
 func _ready():
-	$"../SpellPanel/SpellContainer/SpellsList".connect("spell_selected", self, "_on_spell_selected")
+	$"../SpellPanel/SpellsList".connect("spell_selected", self, "_on_spell_selected")
 
 
 func _on_SelectedSpell_pressed():
 	hide()
 
 
-func _on_spell_selected():
+func _on_spell_selected(spell_name):
+	get_child(0).text = spell_name
 	show()
--- ui/hud/SpellsList.gd
+++ ui/hud/SpellsList.gd
@@ -3,12 +3,13 @@ extends VBoxContainer
 signal spell_selected
 
 
-func _on_Button_toggled(button_pressed):
-	if button_pressed:
-		show()
-	else:
-		hide()
+func _ready():
+	for button in get_children():
+		button.connect("pressed", self, "_on_spell_selected",[button])
 
 
-func _on_spell_selected():
-	emit_signal("spell_selected")
+func _on_spell_selected(button):
+	for other_button in get_children():
+		if not other_button == button:
+			other_button.pressed = false
+	emit_signal("spell_selected", button.text)
