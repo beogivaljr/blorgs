fe8b4878ea2090ec81a3f36fd4acd0f6c0d828b6
VictorDMC <78706864+VictorDMC@users.noreply.github.com>
2021-11-24 20:06:01 -0300

    Adicionado ServerConnection  e modificado world_rpc
    world_rpc agora possui uma função para criar a partida e outra para pegar o id da partida a partir de um hash code
    
    Co-Authored-By: gordominossi <gabriel.minossi@gmail.com>

--- nakama/modules/world_rpc.lua
+++ nakama/modules/world_rpc.lua
@@ -1,15 +1,23 @@
 local nakama = require("nakama")
 
-local function get_world_id(_, _)
-    local matches = nakama.match_list()
-    local current_match = matches[1]
+local matches
 
-    if current_match == nil then
-        return nakama.match_create("world_control", {})
+local function get_world_id(_context, payload)
+	local match_id = matches[payload]
+    if match_id == nil then
+        return -1
     else
-        return current_match.match_id
+        return match_id
     end
 end
 
-
+local function create_world(_context, _payload)	
+	local match_id = nakama.match_create("world_control", {})
+	local hash_code = match_id:sub(8)
+	matches[hash_code] = match_id
+	
+	return hash_code
+	
+end
 nakama.register_rpc(get_world_id, "get_world_id")
+nakama.register_rpc(create_world, "create_world")
