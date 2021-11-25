8236be7d6c5e25bb7a060b42383d4ba74dc2d0da
Gabriel Minossi <gabriel.minossi@gmail.com>
2021-11-24 23:58:48 -0300

    Adding spells table to server state table Putting match_id hash_code in upper case

--- nakama/modules/world_rpc.lua
+++ nakama/modules/world_rpc.lua
@@ -1,23 +1,20 @@
 local nakama = require("nakama")
 
-local matches = {}
+local match_ids = {}
 
 local function get_world_id(_context, payload)
-	local match_id = matches[payload]
-    if match_id == nil then
-        return -1
-    else
-        return match_id
-    end
+    local match_id = match_ids[payload]
+    return match_id and match_id or -1
 end
 
-local function create_world(_context, _payload)	
-	local match_id = nakama.match_create("world_control", {})
-	local hash_code = string.sub(match_id, 1, 8)
-	matches[hash_code] = match_id
-	
-	return hash_code
-	
+local function create_world(_context, _payload)
+    local match_id = nakama.match_create("world_control", {})
+    local hash_code = string.sub(match_id, 1, 8)
+    hash_code = string.upper(hash_code)
+    match_ids[hash_code] = match_id
+
+    return hash_code
 end
+
 nakama.register_rpc(get_world_id, "get_world_id")
 nakama.register_rpc(create_world, "create_world")
