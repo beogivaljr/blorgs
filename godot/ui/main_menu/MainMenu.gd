f606bd5356f1f9bc2a36f8b20be599c187385793
Gabriel Minossi <gabriel.minossi@gmail.com>
2021-11-26 17:24:21 -0300

    Adding player_list_updated signal to MainMenu for match connecting

--- godot/ui/main_menu/MainMenu.gd
+++ godot/ui/main_menu/MainMenu.gd
@@ -8,16 +8,19 @@ func _on_MainButtons_connect_to_game():
 
 
 func _on_MainButtons_new_game():
-	## TODO: Move server/client game conection and creation out of the ScreenContainer
-	yield(ServerConnection.authenticate_async("Jogador"), "completed")
+	GameState.player_type = GameState.PlayerTypes.A
+	yield(ServerConnection.authenticate_async("Jogador1"), "completed")
 	yield(ServerConnection.connect_to_server_async(), "completed")
-	var hash_code = yield(ServerConnection.create_world_async(), "completed")
-	print(hash_code)
-	yield(ServerConnection.join_world_async(hash_code), "completed")
-	$ScreensContainer/NewGameInfo.set_hash_code(hash_code)
-	$ScreensContainer/NewGameInfo.create_new_game()
-	get_tree().change_scene("res://levels/LevelManager.tscn")
+	var match_code = yield(ServerConnection.create_match_async(), "completed")
+	$ScreensContainer/NewGameInfo.set_game_code(match_code)
+	print(match_code)
+	yield(ServerConnection.join_match_async(match_code), "completed")
+	ServerConnection.connect("player_list_updated", self, "_on_player_list_updated")
 	_set_screen(NEW_GAME_INFO)
+
+
+func _on_player_list_updated():
+	get_tree().change_scene("res://levels/LevelManager.tscn")
 
 
 func _on_NewGameInfo_on_cancelled() -> void:
@@ -30,27 +33,18 @@ func _on_ConnectToGameInfo_play_pressed(code, playerName) -> void:
 
 
 func _set_screen(screen):
+	for child in $ScreensContainer.get_children():
+		child.visible = false
+
 	match screen:
 		MAIN_BUTTONS:
 			$ScreensContainer/MainButtons.visible = true
-			$ScreensContainer/NewGameInfo.visible = false
-			$ScreensContainer/ConnectToGameInfo.visible = false
-			$ScreensContainer/Connecting.visible = false
 		NEW_GAME_INFO:
-			$ScreensContainer/MainButtons.visible = false
 			$ScreensContainer/NewGameInfo.visible = true
-			$ScreensContainer/ConnectToGameInfo.visible = false
-			$ScreensContainer/Connecting.visible = false
 		CONNECT_TO_GAME_INFO:
-			$ScreensContainer/MainButtons.visible = false
-			$ScreensContainer/NewGameInfo.visible = false
 			$ScreensContainer/ConnectToGameInfo.visible = true
-			$ScreensContainer/Connecting.visible = false
 		CONNECTING:
-			$ScreensContainer/MainButtons.visible = false
-			$ScreensContainer/NewGameInfo.visible = false
 			$ScreensContainer/ConnectToGameInfo.visible = false
-			$ScreensContainer/Connecting.visible = true
 
 
 func _on_Connecting_canceled() -> void:
--- godot/ui/main_menu/connect_to_game/ConnectToGameInfo.gd
+++ godot/ui/main_menu/connect_to_game/ConnectToGameInfo.gd
@@ -4,9 +4,10 @@ signal play_pressed(code, playerName)
 
 
 func _on_PlayButton_pressed() -> void:
+	GameState.player_type = GameState.PlayerTypes.B
 	var game_code = $GameCodeLineEdit.text
 	var player_name = $PlayerName.text
 	yield(ServerConnection.authenticate_async("Jogador2"), "completed")
 	yield(ServerConnection.connect_to_server_async(), "completed")
-	yield(ServerConnection.join_world_async(game_code), "completed")
+	yield(ServerConnection.join_match_async(game_code), "completed")
 	emit_signal("play_pressed", game_code, player_name)
