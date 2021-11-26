a2accce978d2f7d6eb95c301a738862a3c6ef814
Gabriel Minossi <gabriel.minossi@gmail.com>
2021-11-26 17:15:54 -0300

    Adding parse to dictionary functions to spell data objects

--- godot/data_objects/SpellName.gd
+++ godot/data_objects/SpellName.gd
@@ -3,3 +3,12 @@ extends Node
 
 var function_name: String
 var parameter_name: String
+
+
+func _init(spell_name_dict: Dictionary):
+	function_name = spell_name_dict.function_name
+	parameter_name = spell_name_dict.parameter_name
+
+
+func dict():
+	return {function_name = function_name, parameter_name = parameter_name}
