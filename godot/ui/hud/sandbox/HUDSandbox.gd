0c77bcc0186ae628ef074d8dde5c3d70ab13aeb4
Gabriel Minossi <gabriel.minossi@gmail.com>
2021-11-29 13:02:12 -0300

    Adding spell queue panel to the right and moving spells panel to the left

--- godot/ui/hud/sandbox/HUDSandbox.gd
+++ godot/ui/hud/sandbox/HUDSandbox.gd
@@ -6,33 +6,39 @@ signal player_ready(spells)
 
 var _spells = []
 var _active_spell: SpellDTO = null
+var _puzzle_mode: bool = false
+onready var _spells_list = get_node(
+	"HamburgerContainer/SpellPanel/VBoxContainer/ScrollContainer/SpellsList"
+)
+onready var _spells_queue = get_node("SpellPanel/VBoxContainer/ScrollContainer/SpellsList")
 
 
 func setup(spells, puzzle_mode: bool = false):
-	if puzzle_mode:
+#	_puzzle_mode = true
+	if _puzzle_mode:
 		$SelectedSpellPanelContainer/SelectedSpell.set_text("Coloque os feitiços na lista")
-	_setup_spells_list(spells, puzzle_mode)
+		$SpellPanel/VBoxContainer/ReadyButton.hide()
+	_setup_spells_list(spells)
 
 
 func update_spells_queue(spells_queue):
 	_spells = spells_queue
 
 
-func _setup_spells_list(spells, puzzle_mode: bool = false):
-	var spells_list = $SpellPanel/VBoxContainer/ScrollContainer/SpellsList
-	if not puzzle_mode:
+func _setup_spells_list(spells):
+	if not _puzzle_mode:
 		_spells = spells
-	for container in spells_list.get_children():
-		spells_list.remove_child(container)
+	for container in _spells_list.get_children():
+		_spells_list.remove_child(container)
 		container.queue_free()
 
 	for spell in spells:
 		var spell_container: SpellContainer = spellContainer.instance()
-		spell_container.setup(spell, puzzle_mode)
+		spell_container.setup(spell, _puzzle_mode)
 
-		spell_container.connect("spell_selected", spells_list, "_on_spell_selected")
+		spell_container.connect("spell_selected", _spells_list, "_on_spell_selected")
 		spell_container.connect(
-			"spell_container_button_pressed", spells_list, "_on_spell_container_button_pressed"
+			"spell_container_button_pressed", _spells_list, "_on_spell_container_button_pressed"
 		)
 
 		spell_container.connect(
@@ -53,11 +59,11 @@ func _setup_spells_list(spells, puzzle_mode: bool = false):
 
 		spell_container.connect("spell_selected", self, "_on_spell_selected")
 
-		spells_list.add_child(spell_container)
+		_spells_list.add_child(spell_container)
 
 
 func on_spell_started(_spell_id):
-	$SpellPanel/VBoxContainer/ScrollContainer/SpellsList.disable_buttons()
+	_spells_list.disable_buttons()
 	$SelectedSpellPanelContainer.hide()
 
 
@@ -66,7 +72,7 @@ func on_spell_done(succeded = true):
 		var spell_name = _active_spell.spell_name.function_name if _active_spell else ""
 		$SelectedSpellPanelContainer.display_failed_spell_message(spell_name)
 
-	$SpellPanel/VBoxContainer/ScrollContainer/SpellsList.enable_buttons()
+	_spells_list.enable_buttons()
 
 
 func _on_spell_selected(spell: SpellDTO):
@@ -79,8 +85,8 @@ func _on_spell_selected(spell: SpellDTO):
 
 func _on_ReadyButton_toggled(button_pressed: bool):
 	if button_pressed:
-		$SpellPanel/VBoxContainer/ScrollContainer/SpellsList.disable_buttons()
+		_spells_list.disable_buttons()
 		emit_signal("player_ready", _spells)
 	else:
-		$SpellPanel/VBoxContainer/ScrollContainer/SpellsList.enable_buttons()
+		_spells_list.enable_buttons()
 		emit_signal("player_ready", null)
